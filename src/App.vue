ad14c0653d306a55b14f32fa9aece1e4091e8218
”Jontsev” <vioncev1@gmail.com>
2021-07-04 21:00:38 +0500

    startVue

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+    <div id="text">
+        <h3> {{ text }} </h3>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'app',
+    data() {
+        return {
+            text: 'Welcome'
+        }
+    }
+}
+</script>
+ <style>
+    #text {
+        color: brown;
+    }
+ </style>
